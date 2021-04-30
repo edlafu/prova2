ef60185bb9c7cb901066a2a02e6cc98ddb0ea6e0
edlafu <elarafuentes@gmail.com>
2021-04-30 13:01:01 +0200

    problema con la impresión del contador en el html

--- js/countdown.js
+++ js/countdown.js
@@ -12,35 +12,19 @@
 function processData() {
   // var data = document.getElementById("time").value; classmode
   var data = $("#time").val(); //jqery mode to acces an id in the html5
-  // alert(data);
-  alert (data);
-  return data;
-
-}
-// function processData() {return(data)}; // Timer
-  // var redirect = "/"; // Target URL
-
-  // function countDown() {
-  //   var timer = document.getElementById("timer"); // Timer ID3
-  //   var time = document.getElementById("time");
-  //   if (count > 0) {
-  //     count--;
-  //     timer.innerHTML = "Tu contador se ha puesto a " + count + " segundos."; // Timer Message
-  //     setTimeout("countDown()", 1000); // aqui pasamos valores.
-  //   } else {
-  //     window.location.href = redirect;
-  //   }
-  // }
-  var myTimer;
-  var count = processData;
-
-  function clock() {
+  if (data != null){
+    clock(data);
+  }
+  // return data;
+  }
+///////////////////////////////////////////////////////////
+  function clock(data) {
+    console.log(data);
       myTimer = setInterval(myClock, 1000);
-      var c = processData; //Initially set to 1 hour
-  
-  
+      var c = data; //Initially set to 1 hour
+          
       function myClock() {
-          --c
+          c--;
           var timer = document.getElementById("timer"); // donde mostraremos el contador
           var seconds = c % 60; // Seconds that cannot be written in minutes
           var secondsInMinutes = (c - seconds) / 60; // Gives the seconds that COULD be given in minutes
@@ -48,12 +32,11 @@ function processData() {
           var hours = (secondsInMinutes - minutes) / 60;
           // Now in hours, minutes and seconds, you have the time you need.
           console.clear();
-          console.log(hours + ":" + minutes + ":" + seconds)
-          timer.innerHTML = "Tu contador se ha puesto a " + count + " segundos.";
+          console.log(hours + ":" + minutes + ":" + seconds);
+          // timer.innerHTML = "Tu contador se ha puesto a " hours + ":" + minutes + ":" + seconds+ " segundos.";
           if (c == 0) {
-              clearInterval(myTimer);
+              clearInterval(myTimer); // fin del temporizador.
           }
       }
-  }
-
-  
+    }
+/////////////////////////////////////////////////////////////
